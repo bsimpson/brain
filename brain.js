a3e8e3c98db2a1a6239fed0bc9c5ed220d6a0c08
Ben Simpson <thehoagie@gmail.com>
2018-04-06 10:25:17 -0400

    Improve output message

--- brain.js
+++ brain.js
@@ -41,7 +41,27 @@ function train(data, previousData) {
 }
 
 function execute(input) {
-  return trainedNet(encode(input));
+  let data = trainedNet(encode(input));
+  let topResult = Object.keys(data).reduce((sum, key) => {
+    sum.push([key, data[key]]);
+    return sum;
+  }, [])
+    .sort((a,b) => {
+    if (a[1] > b[1]) {
+      return -1;
+    } else if (a[1] < b[1]) {
+      return 1;
+    } else {
+      return 0;
+    }
+    })
+    [0];
+
+  if (isNaN(topResult[1])) {
+    return `I can't say`
+  }
+
+  return `I'm ${(topResult[1] * 100).toFixed(2)}% confident that ${topResult[0]} said that`;
 }
 
 fs.stat('./training.json', (err, stat) => {
